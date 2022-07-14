a38d85ab6560539d602d29c2c12ac8bf493d8cc7
Ankit Solanki <ankit_ms95@yahoo.com>
2022-07-15 00:36:46 +0530

    manual block call on button click

--- app/src/main/java/com/ankit/restrictcalls/MainActivity.java
+++ app/src/main/java/com/ankit/restrictcalls/MainActivity.java
@@ -1,11 +1,17 @@
 package com.ankit.restrictcalls;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,6 +59,28 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "Permission NOT granted: " + PERMISSION_REQUEST_READ_PHONE_STATE, Toast.LENGTH_SHORT).show();
                 }
             }
+        }
+    }
+
+    public void onTapToBlockCall(View view) {
+        // manual
+        try {
+            TelecomManager tm = (TelecomManager) getSystemService(Context.TELECOM_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ANSWER_PHONE_CALLS) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                Log.e("isCallEnded", tm.endCall() + "");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
